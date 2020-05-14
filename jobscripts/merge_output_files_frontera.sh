da5ff362c036c32731fb4c6a58c80875bcf8ff61
Aaron D. Milstein <neurosutras@gmail.com>
2020-05-14 12:43:34 -0700

    20200514:  - Adding jobscript to merge temp output files on Frontera.

---
+++ jobscripts/merge_output_files_frontera.sh
@@ -0,0 +1,22 @@
+#!/bin/bash -l
+export DATE=$(date +%Y%m%d_%H%M%S)
+export JOB_NAME=merge_output_files_"$2"_"$DATE"
+export MERGE_FILE_PATH="$1"
+sbatch <<EOT
+#!/bin/bash -l
+#SBATCH -J $JOB_NAME
+#SBATCH -o /scratch1/06441/aaronmil/src/optimize_simple_network/logs/$JOB_NAME.%j.o
+#SBATCH -e /scratch1/06441/aaronmil/src/optimize_simple_network/logs/$JOB_NAME.%j.e
+#SBATCH -p development
+#SBATCH -N 1
+#SBATCH -n 1
+#SBATCH -t 2:00:00
+#SBATCH --mail-user=aaronmil@stanford.edu
+#SBATCH --mail-type=BEGIN,END,FAIL
+
+set -x
+
+cd $SCRATCH/src/optimize_simple_network
+
+ibrun -n 1 python3 merge_output_files.py --merge-file-path=$MERGE_FILE_PATH --verbose
+EOT
