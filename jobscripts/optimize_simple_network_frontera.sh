7e79fbb53e16cdf4806cc6fa2531a8601da7b69d
Aaron D. Milstein <neurosutras@gmail.com>
2020-06-10 12:26:05 -0700

    20200610:  - Preparing to run multiple trials each for multiple instances of each network configuration.

--- jobscripts/optimize_simple_network_frontera.sh
+++ jobscripts/optimize_simple_network_frontera.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -l
 export DATE=$(date +%Y%m%d_%H%M%S)
-export JOB_NAME=optimize_simple_network_"$1"_"$DATE"
-export CONFIG_FILE=optimize_simple_network_"$1"_"$2"_config.yaml
+export LABEL="$2"
+export JOB_NAME=optimize_simple_network_"$LABEL"_"$DATE"
+export CONFIG_FILE_PATH="$1"
 sbatch <<EOT
 #!/bin/bash -l
 #SBATCH -J $JOB_NAME
 #SBATCH -o /scratch1/06441/aaronmil/src/optimize_simple_network/logs/$JOB_NAME.%j.o
 #SBATCH -e /scratch1/06441/aaronmil/src/optimize_simple_network/logs/$JOB_NAME.%j.e
 #SBATCH -p normal
-#SBATCH -N 400
-#SBATCH -n 22400
+#SBATCH -N 40
+#SBATCH -n 2240
 #SBATCH -t 6:00:00
 #SBATCH --mail-user=aaronmil@stanford.edu
 #SBATCH --mail-type=BEGIN,END,FAIL
@@ -18,6 +19,6 @@ set -x
 
 cd $SCRATCH/src/optimize_simple_network
 
-ibrun -n 22400 python3 -m nested.optimize --config-file-path=config/$CONFIG_FILE \
+ibrun -n 2240 python3 -m nested.optimize --config-file-path=$CONFIG_FILE_PATH \
     --output-dir=data --pop_size=200 --max_iter=50 --path_length=3 --disp --procs_per_worker=112
 EOT
