660d5ccaeecdae20bc8eeb5b7ebedf57cbf1d1d3
neurosutras <atasbroth.22>
2020-10-21 16:25:44 -0400

    20201021:  - Processing updated network J_reward on Frontera.

--- jobscripts/batch_submit_merge_output_files_frontera.sh
+++ jobscripts/batch_submit_merge_output_files_frontera.sh
@@ -1,11 +1,11 @@
 #!/bin/bash -l
 export DATA_DIR=$SCRATCH/data/optimize_simple_network
-declare -a labels=(J_replay_0 J_replay_1 J_replay_2 J_replay_3 J_replay_4)
-declare -a paths=($DATA_DIR/20200821_151323_simple_network_replay_J_0_exported_output.yaml
-                  $DATA_DIR/20200821_151334_simple_network_replay_J_1_exported_output.yaml
-                  $DATA_DIR/20200821_151334_simple_network_replay_J_2_exported_output.yaml
-                  $DATA_DIR/20200821_151334_simple_network_replay_J_3_exported_output.yaml
-                  $DATA_DIR/20200821_151337_simple_network_replay_J_4_exported_output.yaml)
+declare -a labels=(J_replay_1 J_replay_2 J_replay_3 J_replay_4 J_replay_5)
+declare -a paths=($DATA_DIR/20201021_141037_simple_network_replay_J_reward_1_exported_output.yaml
+                  $DATA_DIR/20201021_142106_simple_network_replay_J_reward_2_exported_output.yaml
+                  $DATA_DIR/20201021_142412_simple_network_replay_J_reward_3_exported_output.yaml
+                  $DATA_DIR/20201021_150420_simple_network_replay_J_reward_4_exported_output.yaml
+                  $DATA_DIR/20201021_150419_simple_network_replay_J_reward_5_exported_output.yaml)
 arraylength=${#labels[@]}
 
 for ((i=0; i<${arraylength}; i++))
