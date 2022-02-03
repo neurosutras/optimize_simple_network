026cccee361d279bf0b2b58a4bef69cf46e58ed7
Aaron D. Milstein <neurosutras@gmail.com>
2022-02-03 16:56:19 -0500

    20220203:  - Processing additional simulations and analysis for manuscript revision.

--- jobscripts/simulate_simple_network_replay_frontera.sh
+++ jobscripts/simulate_simple_network_replay_frontera.sh
@@ -1,12 +1,13 @@
 #!/bin/bash -l
 export DATE=$(date +%Y%m%d_%H%M%S)
-export LABEL=simple_network_replay_"$2"_"$3"_"$4"
-export JOB_NAME=simulate_"$LABEL"_"$DATE"
 export CONFIG_FILE_PATH="$1"
+export MODEL_LABEL="$2"
 export NETWORK_ID="$3"
-export NETWORK_INSTANCE="$4"
+export NETWORK_INSTANCE_START="$4"
 export PARAM_FILE_PATH="$5"
 export MODEL_KEY="$6"
+export LABEL=simple_network_replay_"$MODEL_LABEL"_"$NETWORK_ID"
+export JOB_NAME=simulate_"$LABEL"_"$DATE"
 sbatch <<EOT
 #!/bin/bash -l
 #SBATCH -J $JOB_NAME
@@ -15,7 +16,7 @@ sbatch <<EOT
 #SBATCH -p normal
 #SBATCH -N 20
 #SBATCH -n 1120
-#SBATCH -t 1:00:00
+#SBATCH -t 2:00:00
 #SBATCH --mail-user=neurosutras@gmail.com
 #SBATCH --mail-type=BEGIN,END,FAIL
 
@@ -23,8 +24,14 @@ set -x
 
 cd $WORK2/optimize_simple_network
 
-ibrun -n 1120 python3 simulate_simple_network_replay.py --config-file-path=$CONFIG_FILE_PATH --verbose=1 \
-    --procs_per_worker=112 --export --num_trials=1000 --param_file_path=$PARAM_FILE_PATH \
-    --model_key=$MODEL_KEY --network_id=$NETWORK_ID --network_instance=$NETWORK_INSTANCE --label=$LABEL \
-    --merge-output-files --output-dir=$SCRATCH/data/optimize_simple_network
+let "NETWORK_INSTANCE_END = $NETWORK_INSTANCE_START + 5"
+
+for ((NETWORK_INSTANCE=$NETWORK_INSTANCE_START;NETWORK_INSTANCE<$NETWORK_INSTANCE_END;NETWORK_INSTANCE++))
+do
+  export NETWORK_INSTANCE
+  ibrun -n 1120 python3 simulate_simple_network_replay.py --config-file-path=$CONFIG_FILE_PATH --verbose=1 \
+      --procs_per_worker=112 --export --num_trials=1000 --param_file_path=$PARAM_FILE_PATH \
+      --model_key=$MODEL_KEY --network_id=$NETWORK_ID --network_instance=$NETWORK_INSTANCE \
+      --label="$LABEL"_"$NETWORK_INSTANCE --merge-output-files --output-dir=$SCRATCH/data/optimize_simple_network
+done
 EOT
